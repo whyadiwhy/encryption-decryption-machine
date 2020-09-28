06ec257aa8681200ec81f6922ba8b36bb13813dc
Aditya Gupta <adityagupta22081999@gmail.com>
2020-09-28 19:18:20 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-def
+def adi_machine():
+    keys = 'abcdefgjhijklmnopqrstuvwxyz !'
+    values = keys[-1]+ keys[0:-1]
+    
+    encryptDict = dict(zip(keys,values))
+    decryptDict = dict(zip(values,keys))
+    message = input("Please enter your secret message: ")
+    mode = input("Please input the mode: Encode(E) or Decode(D) ")
+
+    if mode.upper() == 'E':
+        newMessage = ''.join([encryptDict[letter] for letter in message.lower()])
+    elif mode.upper() == 'D':
+        newMessage = ''.join([decryptDict[letter] for letter in message.lower()])
+    else:
+        print("Please Enter a correct Choice")
+
+    return newMessage
+print(adi_machine())
+
